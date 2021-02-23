46bdca804906937af0db2e42d99d5893032f5186
Mónica García <Mnica Garca@LAPTOP-I44AGG1G>
2021-02-23 17:39:26 +0100

    clases

---
+++ Mi primer WPF/VideoClub_Madison/CompactDisc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoClub_Madison
+{
+    public class CompactDisc : Producto
+    {
+        /// <summary>
+        /// Atributos de la clase
+        /// </summary>
+        private string Artista;
+        private string Discografica;
+
+        /// <summary>
+        /// Contrudctor con parámetros
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="precio"></param>
+        /// <param name="descripcion"></param>
+        /// <param name="artista"></param>
+        /// <param name="discografica"></param>
+        public CompactDisc(int codigo, double precio, string descripcion, string artista, string discografica) : base(codigo, precio, descripcion)
+        {
+            Artista = artista;
+            Discografica = discografica;
+        }
+        public string PArtista
+        {
+            get { return Artista; }
+            set { Artista = value; }
+        }
+        public string PDiscografica
+        {
+            get { return Discografica; }
+            set { Discografica = value; }
+        }
+
+    }
+}
---
+++ Mi primer WPF/VideoClub_Madison/Libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoClub_Madison
+{
+    public class Libro : Producto
+    {
+        private string Autor;
+        private string Isbn;
+        public Libro(int codigo, double precio, string descripcion, string autor, string isbn) : base(codigo, precio, descripcion)
+        {
+            Autor = autor;
+            Isbn = isbn;
+        }
+        public string MAutor
+        {
+            get { return Autor; }
+            set { Autor = value; }
+        }
+        public string MIsbn
+        {
+            get { return Isbn; }
+            set { Isbn = value; }
+        }
+
+
+    }
+}
